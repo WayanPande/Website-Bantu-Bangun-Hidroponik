d94a256593b219d892f9ca83b615ae653ff25ecf
WayanPande <yande554@gmail.com>
2021-11-02 09:38:43 +0800

    router loading bar added

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,28 @@ import { Provider } from 'react-redux'
 import { Provider as SessionProvider } from 'next-auth/client'
 import store from '../store'
 import '../styles/globals.css'
+import NProgress from "nprogress"
+import Head from "next/head"
+import Router from "next/router"
+
+Router.onRouteChangeStart = url => {
+  NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => NProgress.done()
+
+Router.onRouteChangeError = () => NProgress.done()
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <SessionProvider session={pageProps.session} >
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          />
+        </Head>
         <Component {...pageProps} />
       </SessionProvider>
     </Provider>
